cf38f56949cf3df29abcdc7a3e0dc379661988ff
Etan Cohen <etancohen@google.com>
2017-03-08 16:43:38 -0800

    sdm845-common: Declare WiFi Aware support to WiFi HAL
     * The Wi-Fi Aware feature added in Android 8.0 enables
       supporting devices to discover, connect, and range
       (added in Android 9) to one another directly using
       the Wi-Fi Aware protocol without internet or cellular
       network access.
    
     * This feature, built upon the Wi-Fi Alliance (WFA) Wi-Fi
       Aware specification (versions 2.0 and 3.0), allows easy
       sharing of high-throughput data among trusted devices
       and apps that are otherwise off-network.
    
       https://source.android.com/devices/tech/connect/wifi-aware
    
    Change-Id: I18ff20e7466b8e8fd598381b730fb7db391e9ad4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -464,6 +464,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
+    
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
